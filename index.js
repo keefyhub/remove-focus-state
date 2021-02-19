006f9ab3102de409ef7a242c08b7a4ec586093ca
Keith Light <keith.light1986@gmail.com>
2021-02-19 12:04:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 class RemoveFocusState {
     constructor() {
         window.addEventListener('keydown', this.handleFirstTab);
-        window.addEventListener('mousedown', this.handleMouseDownOnce);
+        window.addEventListener('mousedown', this.handleFirstMouseDown);
     }
 
     handleFirstTab(e) {
-        if (e.keyCode === 9) {
+        const charCode = e.which || e.keyCode;
+        if (charCode === 9) {
             document.body.classList.add('user-is-tabbing');
             document.body.classList.remove('user-is-clicking');
 
             window.removeEventListener('keydown', this.handleFirstTab);
-            window.addEventListener('mousedown', this.handleMouseDownOnce);
+            window.addEventListener('mousedown', this.handleFirstMouseDown);
         }
     }
 
-    handleMouseDownOnce(){
+    handleFirstMouseDown(){
         document.body.classList.remove('user-is-tabbing');
         document.body.classList.add('user-is-clicking');
 
-        window.removeEventListener('mousedown', this.handleMouseDownOnce);
+        window.removeEventListener('mousedown', this.handleFirstMouseDown);
         window.addEventListener('keydown', this.handleFirstTab);
     }
 }
